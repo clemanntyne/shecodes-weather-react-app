dd4f72a7e86bb34e4404290a897661ecb924a6cc
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2022-01-30 16:07:11 -0800

    actually added bootstrap, adding dummy info for place holder

--- src/Weather.js
+++ src/Weather.js
@@ -5,13 +5,29 @@ export default function Weather() {
     <div className="weather">
       <form>
         <input type="search" placeholder="City.." />
-        <button type="submit">Search</button>
+        <button type="submit" className="btn btn-primary">
+          Search
+        </button>
       </form>
       <ul>
         <li>New York</li>
         <li>Last Updated: Today 3:49 pm</li>
         <li>Clouds</li>
       </ul>
+      <div className="row">
+        <div className="col-6">
+          <span>img</span>
+          <span>50</span>
+          <span>°F | °C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Clouds: 42%</li>
+            <li>Wind: 2mph</li>
+            <li>Humidity: 85%</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
