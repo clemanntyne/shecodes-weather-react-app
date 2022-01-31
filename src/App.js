c7b372e92e4aff54553fa86f18dc767ed0ba2df4
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2022-01-30 17:49:35 -0800

    Added OpenSource info at end.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import "./App.css";
 import Weather from "./Weather";
 import Search from "./Search";
 import Forecast from "./Forecast";
+import OpenSource from "./OpenSource";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Search />
-      <Weather />
-      <Forecast />
+      <div className="container">
+        <Search />
+        <Weather />
+        <Forecast />
+      </div>
+      <div>
+        <OpenSource />
+      </div>
     </div>
   );
 }
---
+++ src/OpenSource.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function OpenSource() {
+  return (
+    <div>
+      <p className="ms-3 mt-3">
+        <a href="https://github.com/clemanntyne/shecodes-weather-react-app">
+          Open-Source code
+        </a>
+        , by <a href="https://friendly-yonath-678f14.netlify.app">Stephanie</a>{" "}
+        from She Codes
+      </p>
+    </div>
+  );
+}
